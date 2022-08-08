7ab69f8f4d281f6489a32bf48fffa88afd2be1b4
TaraHelberg <taralhelberg@outlook.com>
2022-08-08 12:38:06 +0000

    Add text to You loose message in run_ game function

--- run.py
+++ run.py
@@ -200,7 +200,7 @@ def run_game(word, num_lives):
     if game_over:
         print(f"{Fore.GREEN+Style.BRIGHT}Congratulations! YOU WON !")
     else:
-        print(f"{Fore.RED+Style.BRIGHT}Sorry :( You Loose"
+        print(f"{Fore.RED+Style.BRIGHT}Sorry :( You Loose !! "
               f"The word you had to Guess was {word}")
 
     restart_game()
